d7d6193a390121c25648b9d7dcfab382372efaba
SonOfMagic <qq1324318532@gmail.com>
2022-02-21 11:35:17 +0800

    feat: init project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-export const foo = 'bar'
-
-export const wait = (timeout?: number) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true)
-    }, timeout)
-  })
+export default function replace (
+  str: string,
+  pattern: RegExp,
+  replacement:
+    | string
+    | ((
+        match: string,
+        arr: RegExpExecArray,
+        index: number,
+        lastIndex: number,
+        string: string
+      ) => string)
+) {
+  const isString = typeof replacement === 'string'
+  const isFn = typeof replacement === 'function'
+  if (!isString && !isFn) {
+    throw new TypeError('replacement must be a String or Fn return String')
+  }
+  let p = 0
+  let arr: RegExpExecArray | null
+  const res = []
+  const fn = isString ? () => replacement : replacement
+  while ((arr = pattern.exec(str)) !== null) {
+    res.push(str.slice(p, arr.index))
+    p = arr.index
+    res.push(
+      // return false like use empty string replace
+      // return '0' instead of 0
+      fn(str.slice(p, pattern.lastIndex), arr, p, pattern.lastIndex, str)
+    )
+    p = pattern.lastIndex
+  }
+  res.push(str.slice(p))
+  return res.join('')
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,30 @@
-import { foo, wait } from '../src'
+import replace from '../src'
 
 describe('[Default]', () => {
-  test('foo should be bar', () => {
-    expect(foo).toBe('bar')
+  test('Default return self', () => {
+    const testCase =
+      '<view class="p-[20px] -mt-2 mb-[-20px]">p-[20px] -mt-2 mb-[-20px] margin的jit 不能这么写 -m-[20px]</view><view class="space-y-[1.6rem]"><view class="w-[300rpx] text-black text-opacity-[0.19]">w-[300rpx] text-black text-opacity-[0.19]</view><view class="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]</view><view class="max-w-[300rpx] min-h-[100px] text-[#dddddd]">max-w-[300rpx] min-h-[100px] text-[#dddddd]</view><view class="flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff]">Hello</view><view class="border-[10px] border-[#098765] border-solid border-opacity-[0.44]">border-[10px] border-[#098765] border-solid border-opacity-[0.44]</view><view class="grid grid-cols-3 divide-x-[10px] divide-[#010101] divide-solid"><view>1</view><view>2</view><view>3</view></view></view><view class="test">test</view>'
+    const regex =
+      /(?:class|className)=(?:["']\W+\s*(?:\w+)\()?["']([^"]+)['"]/gim
+
+    expect(
+      replace(testCase, regex, (x, arr, idx, lastIdx, str) => {
+        expect(str).toBe(testCase)
+        return x
+      })
+    ).toBe(testCase)
   })
 
-  test('wait 100ms', async () => {
-    const flag = await wait(100)
-    expect(flag).toBe(true)
+  test('set empty class ', () => {
+    const testCase =
+      '<view class="p-[20px] -mt-2 mb-[-20px]">p-[20px] -mt-2 mb-[-20px] margin的jit 不能这么写 -m-[20px]</view>'
+    const regex =
+      /(?:class|className)=(?:["']\W+\s*(?:\w+)\()?["']([^"]+)['"]/gim
+    const result = replace(testCase, regex, (x, arr, idx, lastIdx, str) => {
+      return ''
+    })
+    expect(result).toBe(
+      '<view >p-[20px] -mt-2 mb-[-20px] margin的jit 不能这么写 -m-[20px]</view>'
+    )
   })
 })
